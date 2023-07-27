6ed45d54879c6f7987bc0590a8532966a1070046
Martin Schubert <30735893+mfschubert@users.noreply.github.com>
2023-07-27 10:26:26 -0700

    Add incident beam example

--- examples/crystal.py
+++ examples/crystal.py
@@ -8,7 +8,7 @@ from typing import Tuple
 
 import jax.numpy as jnp
 
-from fmmax import basis, fields, fmm, layer, scattering, sources
+from fmmax import basis, beams, fields, fmm, layer, scattering, sources
 
 
 def simulate_crystal_with_internal_source(
@@ -143,6 +143,192 @@ def simulate_crystal_with_internal_source(
         s_matrices_interior_after_source=s_matrices_interior_after_source,
         backward_amplitude_before_end=bwd_amplitude_before_end,
         forward_amplitude_after_start=fwd_amplitude_after_start,
+    )
+    (ex, ey, ez), (hx, hy, hz), (x, y, z) = fields.stack_fields_3d_auto_grid(
+        amplitudes_interior=amplitudes_interior,
+        layer_solve_results=[
+            solve_result_ambient,
+            solve_result_crystal,
+            solve_result_crystal,
+            solve_result_ambient,
+        ],
+        layer_thicknesses=[
+            thickness_ambient_,
+            thickness_slab_ / 2,
+            thickness_slab_ / 2,
+            thickness_ambient_,
+        ],
+        resolution=resolution_fields,
+        num_unit_cells=brillouin_grid_shape,
+    )
+
+    # Perform the Brillouin zone integration by averaging over the Brillouin zone
+    # grid batch axes.
+    ex = jnp.mean(ex, axis=(0, 1))
+    ey = jnp.mean(ey, axis=(0, 1))
+    ez = jnp.mean(ez, axis=(0, 1))
+    hx = jnp.mean(hx, axis=(0, 1))
+    hy = jnp.mean(hy, axis=(0, 1))
+    hz = jnp.mean(hz, axis=(0, 1))
+
+    # Compute some cross sections for visualizing the structure.
+    section_xy, section_xz, section_yz = crystal_cross_sections(
+        thickness_ambient=float(thickness_ambient_),
+        thickness_slab=float(thickness_slab_),
+        pitch=pitch,
+        diameter=diameter,
+        resolution=resolution,
+        num_unit_cells=brillouin_grid_shape,
+    )
+    return (ex, ey, ez), (hx, hy, hz), (x, y, z), (section_xy, section_xz, section_yz)
+
+
+def simulate_crystal_with_gaussian_beam(
+    polar_angle: float = 0.0,
+    azimuthal_angle: float = 0.0,
+    polarization_angle: float = 0.0,
+    permittivity_ambient: complex = (1.0 + 0.0j) ** 2,
+    permittivity_slab: complex = (1.5 + 0.0j) ** 2,
+    thickness_ambient: float = 2.0,
+    thickness_slab: float = 0.4,
+    pitch: float = 1.0,
+    diameter: float = 0.7,
+    resolution: float = 0.01,
+    resolution_fields: float = 0.05,
+    wavelength: float = 0.63,
+    approximate_num_terms: int = 50,
+    brillouin_grid_shape: Tuple[int, int] = (5, 5),
+) -> Tuple[
+    Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray],  # (ex, ey, ez)
+    Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray],  # (hx, hy, hz)
+    Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray],  # (x, y, z)
+    Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray],  # (xy, xz, yz) cross sections
+]:
+    """Simulates a Gaussian beam incident on photonic crystal slab.
+
+    The crystal has a square unit cell with circular holes, having cross section
+    and dipole position as illustrated below.
+                     ________________
+                    |                |
+                    |XX            XX|
+                    |XXXX        XXXX|
+                    |XXXX        XXXX|
+                    |XX            XX|
+                    |________________|
+
+    Args:
+        permittivity_ambient: Permittivity of the region above and below the slab, and
+            of the holes in the slab.
+        permittivity_slab: Permittivity of the slab.
+        thickness_ambient: Thickness of the ambient layers above and below the slab.
+        thickness_slab: Thickness of the photonic crystal slab.
+        pitch: The unit cell pitch.
+        diameter: The diameter of the holes in the photonic crystal.
+        resolution: The size of a pixel in permittivity arrays.
+        resolution_fields: The size of a pixel in field arrays.
+        wavelength: The wavelength, of the dipole emission.
+        approximate_num_terms: The number of terms in the Fourier expansion.
+        brillouin_grid_shape: The shape of the grid used for Brillouin zone integration.
+
+    Returns:
+        The electric and magnetic fields, and the grid coordinates, `((ex, ey, ez),
+        (hx, hy, hz), (x,y, z))`.
+    """
+    thickness_ambient_ = jnp.asarray(thickness_ambient)
+    thickness_slab_ = jnp.asarray(thickness_slab)
+    del thickness_ambient, thickness_slab
+
+    primitive_lattice_vectors = basis.LatticeVectors(
+        u=pitch * basis.X, v=pitch * basis.Y
+    )
+    expansion = basis.generate_expansion(
+        primitive_lattice_vectors=primitive_lattice_vectors,
+        approximate_num_terms=approximate_num_terms,
+        truncation=basis.Truncation.CIRCULAR,
+    )
+
+    # Brillouin zone integration creates a batch of in-plane wavevectors which are
+    # distributed throughout the first Brillouin zone. We shift the expansion so
+    # that it is centered on the direction of the incident beam.
+    in_plane_wavevector = basis.brillouin_zone_in_plane_wavevector(
+        brillouin_grid_shape, primitive_lattice_vectors
+    )
+    in_plane_wavevector += basis.plane_wave_in_plane_wavevector(
+        wavelength=jnp.asarray(wavelength),
+        polar_angle=jnp.asarray(polar_angle),
+        azimuthal_angle=jnp.asarray(azimuthal_angle),
+        permittivity=jnp.asarray(permittivity_ambient),
+    )
+    assert in_plane_wavevector.shape[-1] == 2
+    assert in_plane_wavevector.ndim == 3
+
+    eigensolve = functools.partial(
+        layer.eigensolve_isotropic_media,
+        wavelength=jnp.asarray(wavelength),
+        in_plane_wavevector=in_plane_wavevector,
+        primitive_lattice_vectors=primitive_lattice_vectors,
+        expansion=expansion,
+        formulation=fmm.Formulation.FFT,
+    )
+
+    mask = unit_cell_pattern(pitch, diameter, resolution)
+    permittivity_crystal = jnp.where(mask, permittivity_slab, permittivity_slab)
+    solve_result_crystal = eigensolve(permittivity=permittivity_crystal)
+    solve_result_ambient = eigensolve(
+        permittivity=jnp.asarray(permittivity_ambient)[jnp.newaxis, jnp.newaxis]
+    )
+
+    s_matrices_interior = scattering.stack_s_matrices_interior(
+        layer_solve_results=[
+            solve_result_ambient,
+            solve_result_crystal,
+            solve_result_ambient],
+        layer_thicknesses=[
+            thickness_ambient_,
+            thickness_slab_,
+            thickness_ambient_],
+    )
+
+    # Define a function that gives us the fields for a plane wave. We will 
+    # use this to compute the corresponding amplitudes.
+    def _beam_field_fn(x, y, z):
+        del x, y
+        ex = jnp.exp(1j * 2 * jnp.pi / wavelength * z)
+        ey = jnp.zeros_like(ex)
+        ez = jnp.zeros_like(ex)
+        hx = jnp.zeros_like(ex)
+        hy = ex
+        hz = jnp.zeros_like(ex)
+        return (ex, ey, ez), (hx, hy, hz)
+    
+    x, y = basis.unit_cell_coordinates(
+        primitive_lattice_vectors=primitive_lattice_vectors,
+        shape=permittivity_crystal.shape,
+        num_unit_cells=brillouin_grid_shape,
+    )
+    (beam_ex, beam_ey, _), (beam_hx, beam_hy, _) = beams.rotated_fields(
+        field_fn=_beam_field_fn,
+        x=x,
+        y=y,
+        z=jnp.zeros_like(x),
+        polar_angle=jnp.asarray(polar_angle),
+        azimuthal_angle=jnp.asarray(azimuthal_angle),
+        polarization_angle=jnp.asarray(polarization_angle),
+    )
+    fwd_amplitude, _ = sources.amplitudes_for_fields(
+        ex=beam_ex,
+        ey=beam_ey,
+        hx=beam_hx,
+        hy=beam_hy,
+        layer_solve_result=solve_result_ambient,
+        brillouin_grid_axes=(0, 1),
+    )
+
+    # Compute the fields inside the structure.
+    amplitudes_interior = fields.stack_amplitudes_interior(
+        s_matrices_interior=s_matrices_interior,
+        forward_amplitude_0_start=fwd_amplitude,
+        bwd_amplitude_N_end=jnp.zeros_like(fwd_amplitude),
     )
     (ex, ey, ez), (hx, hy, hz), (x, y, z) = fields.stack_fields_3d_auto_grid(
         amplitudes_interior=amplitudes_interior,
