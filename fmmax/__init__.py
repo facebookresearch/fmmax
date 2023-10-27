26491314cefcc1faa38279c7d9eeb98383c9709e
Alec Hammond <alechammond@fb.com>
2023-10-27 14:11:22 -0700

    fix imports

--- fmmax/__init__.py
+++ fmmax/__init__.py
@@ -1,13 +1,15 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 
-from . import basis
-from . import beams
-from . import farfield
-from . import fft
-from . import fields
-from . import fmm
-from . import fmm_matrices
-from . import scattering
-from . import sources
-from . import utils
-from . import vector
+from . import (
+    basis,
+    beams,
+    farfield,
+    fft,
+    fields,
+    fmm,
+    fmm_matrices,
+    scattering,
+    sources,
+    utils,
+    vector,
+)
