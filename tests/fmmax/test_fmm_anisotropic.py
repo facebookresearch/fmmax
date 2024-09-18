31e512b39db2a316aef0fb57ed24e85f6575e27b
Martin Schubert <martinschubert@Martins-MBP.attlocal.net>
2024-09-18 12:44:21 -0700

    simplify repro

--- tests/fmmax/test_fmm_anisotropic.py
+++ tests/fmmax/test_fmm_anisotropic.py
@@ -114,15 +114,9 @@ class ReproTest(unittest.TestCase):
     @parameterized.parameterized.expand(
         [
             (fmm.Formulation.FFT, 0.0),
-            # (fmm.Formulation.FFT, jnp.pi / 4),
-            # (fmm.Formulation.FFT, jnp.pi / 2),
-            # (fmm.Formulation.JONES_DIRECT, 0.0),
-            # (fmm.Formulation.JONES_DIRECT, jnp.pi / 2),
-            # (fmm.Formulation.JONES_DIRECT_FOURIER, 0.0),
-            # (fmm.Formulation.JONES_DIRECT_FOURIER, jnp.pi / 2),
         ]
     )
-    def test_reflection_with_anisotropic_eignensolve_matches_isotropic_tight_tolerance(
+    def test_repro(
         self, formulation, grating_angle
     ):
         # Checks that the zeroth order reflection of a grating computed using the anisotropic
