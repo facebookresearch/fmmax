54c8caa27ab9c97831729674873a4ddfd3030644
Martin Schubert <martinschubert@Martins-MBP.attlocal.net>
2024-09-20 15:47:05 -0700

    reset

--- tests/fmmax/test_debug_ci_failures.py
+++ tests/fmmax/test_debug_ci_failures.py
@@ -26,17 +26,30 @@ class ReproTest(unittest.TestCase):
 
 
 def jax_calculation():
+    # Computes the TE- and TM-reflection from a metallic grating.
+    formulation = fmm.Formulation.FFT
+    grating_angle = 0.0
     permittivity_ambient = jnp.asarray([[1.0 + 0.0j]])
     permittivity_passivation = jnp.asarray([[2.25 + 0.0j]])
     permittivity_metal = jnp.asarray([[-7.632 + 0.731j]])
 
-    x, _ = jnp.meshgrid(jnp.linspace(-0.5, 0.5), jnp.linspace(-0.5, 0.5), indexing="ij")
-    density = (-0.2 < x) & (x < 0.2)
+    # Permittivity of the grating layer.
+    x, y = jnp.meshgrid(jnp.linspace(-0.5, 0.5), jnp.linspace(-0.5, 0.5), indexing="ij")
+    position = x * jnp.cos(grating_angle) + y * jnp.sin(grating_angle)
+    density = (-0.2 < position) & (position < 0.2)
     permittivity_grating = utils.interpolate_permittivity(
         permittivity_solid=permittivity_metal,
         permittivity_void=permittivity_passivation,
         density=density,
     )
+
+    # Thickensses of ambient, passivation, grating, and metal substrate.
+    thicknesses = [
+        jnp.asarray(0.0),
+        jnp.asarray(0.02),
+        jnp.asarray(0.06),
+        jnp.asarray(0.0),
+    ]
 
     primitive_lattice_vectors = basis.LatticeVectors(u=basis.X, v=basis.Y)
     expansion = basis.generate_expansion(
@@ -49,15 +62,36 @@ def jax_calculation():
         "in_plane_wavevector": jnp.zeros((2,)),
         "primitive_lattice_vectors": primitive_lattice_vectors,
         "expansion": expansion,
-        "formulation": fmm.Formulation.FFT,
+        "formulation": formulation,
     }
+    solve_result_ambient = fmm.eigensolve_isotropic_media(
+        permittivity=permittivity_ambient, **eigensolve_kwargs
+    )
+    solve_result_passivation = fmm.eigensolve_isotropic_media(
+        permittivity=permittivity_passivation, **eigensolve_kwargs
+    )
     solve_result_metal = fmm.eigensolve_isotropic_media(
         permittivity=permittivity_metal, **eigensolve_kwargs
     )
+
+    # Perform the isotropic grating eigensolve and compute the zeroth-order reflectivity.
     solve_result_grating_isotropic = fmm.eigensolve_isotropic_media(
         permittivity=permittivity_grating, **eigensolve_kwargs
     )
+    s_matrix_isotropic = scattering.stack_s_matrix(
+        layer_solve_results=[
+            solve_result_ambient,
+            solve_result_passivation,
+            solve_result_grating_isotropic,
+            solve_result_metal,
+        ],
+        layer_thicknesses=thicknesses,
+    )
+    n = expansion.num_terms
+    r_te_isotropic = s_matrix_isotropic.s21[0, 0]
+    r_tm_isotropic = s_matrix_isotropic.s21[n, n]
 
+    # Perform the anisotropic grating eigensolve and compute the zeroth-order reflectivity.
     solve_result_grating_anisotropic = fmm.eigensolve_general_anisotropic_media(
         permittivity_xx=permittivity_grating,
         permittivity_xy=jnp.zeros_like(permittivity_grating),
@@ -71,5 +105,16 @@ def jax_calculation():
         permeability_zz=jnp.ones_like(permittivity_grating),
         **eigensolve_kwargs,
     )
+    s_matrix_anisotropic = scattering.stack_s_matrix(
+        layer_solve_results=[
+            solve_result_ambient,
+            solve_result_passivation,
+            solve_result_grating_anisotropic,  # Use results of anisotropic eigensolve.
+            solve_result_metal,
+        ],
+        layer_thicknesses=thicknesses,
+    )
+    r_te_anisotropic = s_matrix_anisotropic.s21[0, 0]
+    r_tm_anisotropic = s_matrix_anisotropic.s21[n, n]
 
     return True
