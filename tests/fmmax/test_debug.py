57620121e3476dcf31355ffe38f12cd4a2c55fc2
Martin Schubert <mfschubert@gmail.com>
2024-10-11 11:06:23 -0700

    eig only

--- tests/fmmax/test_debug.py
+++ tests/fmmax/test_debug.py
@@ -19,21 +19,23 @@ jax.config.update("jax_enable_x64", True)
 class DebugTest(unittest.TestCase):
     def test_simple(self):
 
-        for i in range(10):    
-            primitive_lattice_vectors = basis.LatticeVectors(u=basis.X, v=basis.Y)
-            expansion = basis.generate_expansion(
-                primitive_lattice_vectors=primitive_lattice_vectors,
-                approximate_num_terms=220,
-                truncation=basis.Truncation.CIRCULAR,
-            )
-            eigensolve_kwargs = {
-                "wavelength": jnp.asarray(0.63),
-                "in_plane_wavevector": jnp.zeros((2,)),
-                "primitive_lattice_vectors": primitive_lattice_vectors,
-                "expansion": expansion,
-                "formulation": fmm.Formulation.FFT,
-            }
+        for i in range(10):
+            mat = jax.random.normal((440, 440))
+            utils.eig(mat)
+            # primitive_lattice_vectors = basis.LatticeVectors(u=basis.X, v=basis.Y)
+            # expansion = basis.generate_expansion(
+            #     primitive_lattice_vectors=primitive_lattice_vectors,
+            #     approximate_num_terms=220,
+            #     truncation=basis.Truncation.CIRCULAR,
+            # )
+            # eigensolve_kwargs = {
+            #     "wavelength": jnp.asarray(0.63),
+            #     "in_plane_wavevector": jnp.zeros((2,)),
+            #     "primitive_lattice_vectors": primitive_lattice_vectors,
+            #     "expansion": expansion,
+            #     "formulation": fmm.Formulation.FFT,
+            # }
 
-            solve_result_grating_isotropic = fmm.eigensolve_isotropic_media(
-                permittivity=jnp.ones((50, 50)), **eigensolve_kwargs
-            )
+            # solve_result_grating_isotropic = fmm.eigensolve_isotropic_media(
+            #     permittivity=jnp.ones((50, 50)), **eigensolve_kwargs
+            # )
