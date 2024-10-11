a4167ac7c96d79e70a51cea9de041207d7704f06
Martin Schubert <mfschubert@gmail.com>
2024-10-11 14:05:41 -0700

    improve comments

--- src/fmmax/utils.py
+++ src/fmmax/utils.py
@@ -137,6 +137,8 @@ def eig(
 
 def _eig_jax(matrix: jnp.ndarray) -> Tuple[jnp.ndarray, jnp.ndarray]:
     """Eigendecomposition using `jax.numpy.linalg.eig`."""
+    # If using CPU backend, using `pure_callback` to call a jit-compiled version of
+    # `jnp.linalg.eig` is flaky and can cause deadlocks. Directly call it instead.
     if jax.devices()[0] == jax.devices("cpu")[0]:
         return jnp.linalg.eig(matrix)
     else:
