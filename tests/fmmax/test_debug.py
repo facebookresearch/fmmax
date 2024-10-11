4d472c8415d8cd06268163ab75f86502d4302e9a
Martin Schubert <mfschubert@gmail.com>
2024-10-11 11:10:17 -0700

    really simplify

--- tests/fmmax/test_debug.py
+++ tests/fmmax/test_debug.py
@@ -18,25 +18,5 @@ jax.config.update("jax_enable_x64", True)
 
 class DebugTest(unittest.TestCase):
     def test_simple(self):
-
-        key = jax.random.PRNGKey(0)
         for i in range(10):
-            mat = jax.random.normal(jax.random.fold_in(key, i), (440, 440))
-            utils.eig(mat)
-            # primitive_lattice_vectors = basis.LatticeVectors(u=basis.X, v=basis.Y)
-            # expansion = basis.generate_expansion(
-            #     primitive_lattice_vectors=primitive_lattice_vectors,
-            #     approximate_num_terms=220,
-            #     truncation=basis.Truncation.CIRCULAR,
-            # )
-            # eigensolve_kwargs = {
-            #     "wavelength": jnp.asarray(0.63),
-            #     "in_plane_wavevector": jnp.zeros((2,)),
-            #     "primitive_lattice_vectors": primitive_lattice_vectors,
-            #     "expansion": expansion,
-            #     "formulation": fmm.Formulation.FFT,
-            # }
-
-            # solve_result_grating_isotropic = fmm.eigensolve_isotropic_media(
-            #     permittivity=jnp.ones((50, 50)), **eigensolve_kwargs
-            # )
+            utils.eig(jnp.ones((440, 440)))
