1abc8d0dc6dc476766ec0b9ead1400bea9983ff8
Martin Schubert <mfschubert@gmail.com>
2024-10-11 10:47:08 -0700

    simplify

--- tests/fmmax/test_debug.py
+++ tests/fmmax/test_debug.py
@@ -17,12 +17,9 @@ jax.config.update("jax_enable_x64", True)
 
 
 def jax_calculation():
-    formulation = fmm.Formulation.JONES_DIRECT
-    
     # Computes the TE- and TM-reflection from a metallic grating.
-    permittivity_ambient = jnp.asarray([[1.0 + 0.0j]])
-    permittivity_passivation = jnp.asarray([[2.25 + 0.0j]])
-    permittivity_metal = jnp.asarray([[-7.632 + 0.731j]])
+    permittivity_passivation = jnp.asarray([[1.0]])
+    permittivity_metal = jnp.asarray([[1.0]])
 
     # Permittivity of the grating layer.
     permittivity_grating = utils.interpolate_permittivity(
@@ -42,18 +39,9 @@ def jax_calculation():
         "in_plane_wavevector": jnp.zeros((2,)),
         "primitive_lattice_vectors": primitive_lattice_vectors,
         "expansion": expansion,
-        "formulation": formulation,
+        "formulation": fmm.Formulation.JONES_DIRECT,
     }
 
-    solve_result_ambient = fmm.eigensolve_isotropic_media(
-        permittivity=permittivity_ambient, **eigensolve_kwargs
-    )
-    solve_result_passivation = fmm.eigensolve_isotropic_media(
-        permittivity=permittivity_passivation, **eigensolve_kwargs
-    )
-    solve_result_metal = fmm.eigensolve_isotropic_media(
-        permittivity=permittivity_metal, **eigensolve_kwargs
-    )
     solve_result_grating_isotropic = fmm.eigensolve_isotropic_media(
         permittivity=permittivity_grating, **eigensolve_kwargs
     )
