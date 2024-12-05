17572e5ab4be15f4d3b0b9929a93fb19c18d2cab
Martin Schubert <mfschubert@gmail.com>
2024-12-04 19:33:13 -0800

    dedicated function for s-matrix from two layers

--- src/fmmax/scattering.py
+++ src/fmmax/scattering.py
@@ -155,7 +155,7 @@ def _stack_s_matrices(
     # The initial scattering matrix is just the identity matrix, with the
     # necessary batch dimensions.
     eye = utils.diag(jnp.ones_like(layer_solve_results[0].eigenvalues))
-    s_matrix = ScatteringMatrix(
+    layer_s_matrix_0 = ScatteringMatrix(
         s11=eye,
         s12=jnp.zeros_like(eye),
         s21=jnp.zeros_like(eye),
@@ -165,11 +165,17 @@ def _stack_s_matrices(
         end_layer_solve_result=layer_solve_results[0],
         end_layer_thickness=layer_thicknesses[0],
     )
+    layer_s_matrix_1 = _pair_s_matrix(
+        layer_solve_result=layer_solve_results[0],
+        layer_thickness=layer_thicknesses[0],
+        next_layer_solve_result=layer_solve_results[1],
+        next_layer_thickness=layer_thicknesses[1],
+    )
 
     # TODO(mfschubert): Figure out how to use `jax.lax.fori_loop` or similar.
-    s_matrices = [s_matrix]
+    s_matrices = [layer_s_matrix_0, layer_s_matrix_1]
     for layer_solve_result, layer_thickness in zip(
-        layer_solve_results[1:], layer_thicknesses[1:]
+        layer_solve_results[2:], layer_thicknesses[2:]
     ):
         s_matrices.append(
             append_layer(s_matrices[-1], layer_solve_result, layer_thickness)
@@ -181,7 +187,7 @@ def stack_s_matrix_scan(
     layer_solve_results: fmm.LayerSolveResult,
     layer_thicknesses: jnp.ndarray,
 ) -> ScatteringMatrix:
-    """Computes the stack matrix for a stack of layers.
+    """Computes the scattering matrix for a stack of layers by a scan operation.
 
     Unlike `stack_s_matrix`, this function uses a scan operation rather than a python
     for loop, which can lead to significantly smaller programs and shorter compile times.
@@ -375,11 +381,11 @@ def _extend_s_matrix(
     # term1 = diag(q) @ phi_T @ next_omega_k @ next_phi @ diag(1 / next_q)
     # term1 = q[..., :, jnp.newaxis] * jnp.linalg.solve(
     #     omega_k @ phi,
-    #     next_omega_k @ next_phi * (1 / next_q)[..., jnp.newaxis, :],
+    #     next_omega_k @ next_phi @ diag(1 / next_q),
     # )
-    term1 = utils.diag(q) @ jnp.linalg.solve(
+    term1 = q[..., jnp.newaxis] * jnp.linalg.solve(
         omega_k @ phi,
-        next_omega_k @ next_phi @ utils.diag(1 / next_q),
+        next_omega_k @ next_phi * (1 / next_q)[..., jnp.newaxis, :],
     )
     # term2 = phi_T @ omega_k @ next_phi
     term2 = jnp.linalg.solve(omega_k @ phi, omega_k @ next_phi)
@@ -388,23 +394,73 @@ def _extend_s_matrix(
 
     # Phase terms \hat{f}(d) defined near equation 4.2 of [1999 Whittaker]. These
     # describe phase accumulated by propagating across a layer for each eigenmode.
-    fd = utils.diag(jnp.exp(1j * q * layer_thickness))
-    fd_next = utils.diag(jnp.exp(1j * next_q * next_layer_thickness))
+    fd = jnp.exp(1j * q * layer_thickness)
+    fd_next = jnp.exp(1j * next_q * next_layer_thickness)
 
     # Update the s-matrix to include the present layer, following the recipe
     # given in equation 5.4 of [1999 Whittaker].
     s11, s12, s21, s22 = s_matrix_blocks
 
     # s11_next = inv(i11 - diag(fd) @ s12 @ i21) @ diag(fd) @ s11
-    term3 = i11 - fd @ s12 @ i21
-    s11_next = jnp.linalg.solve(term3, fd @ s11)
+    term3 = i11 - fd[..., jnp.newaxis] * s12 @ i21
+    s11_next = jnp.linalg.solve(term3, fd[..., jnp.newaxis] * s11)
     # s12_next = inv(i11 - diag(fd) @ s12 @ i21) @ (diag(fd) @ s12 @ i22 - i12) @ diag(fd_next)
-    s12_next = jnp.linalg.solve(term3, (fd @ s12 @ i22 - i12) @ fd_next)
+    s12_next = jnp.linalg.solve(
+        term3,
+        (fd[..., jnp.newaxis] * s12 @ i22 - i12) * fd_next[..., jnp.newaxis, :],
+    )
     s21_next = s22 @ i21 @ s11_next + s21
     # s22_next = s22 @ i21 @ s12_next + s22 @ i22 @ diag(fd_next)
-    s22_next = s22 @ i21 @ s12_next + s22 @ i22 @ fd_next
+    s22_next = s22 @ i21 @ s12_next + s22 @ i22 * fd_next[..., jnp.newaxis, :]
 
     return (s11_next, s12_next, s21_next, s22_next)
+
+
+def _pair_s_matrix(
+    layer_solve_result: fmm.LayerSolveResult,
+    layer_thickness: jnp.ndarray,
+    next_layer_solve_result: fmm.LayerSolveResult,
+    next_layer_thickness: jnp.ndarray,
+) -> ScatteringMatrix:
+    """Generate the scattering matrix for a pair of layers."""
+    # Alias for brevity: eigenvalues, eigenvectors, and omega-k matrix.
+    q = layer_solve_result.eigenvalues
+    phi = layer_solve_result.eigenvectors
+    omega_k = layer_solve_result.omega_script_k_matrix
+
+    next_q = next_layer_solve_result.eigenvalues
+    next_phi = next_layer_solve_result.eigenvectors
+    next_omega_k = next_layer_solve_result.omega_script_k_matrix
+
+    term1 = q[..., jnp.newaxis] * jnp.linalg.solve(
+        omega_k @ phi,
+        next_omega_k @ next_phi * (1 / next_q)[..., jnp.newaxis, :],
+    )
+
+    term2 = jnp.linalg.solve(omega_k @ phi, omega_k @ next_phi)
+    i11 = i22 = 0.5 * (term1 + term2)
+    i12 = i21 = 0.5 * (-term1 + term2)
+
+    fd = jnp.exp(1j * q * layer_thickness)
+    fd_next = jnp.exp(1j * next_q * next_layer_thickness)
+
+    # The computation is identical to that in `_extend_s_matrix` with `s11` and `s22`
+    # being the identity, and `s12` and `s21` being zero.
+    s11 = jnp.linalg.solve(i11, utils.diag(fd))
+    s12 = jnp.linalg.solve(i11, -i12 * fd_next[..., jnp.newaxis, :])
+    s21 = i21 @ s11
+    s22 = i21 @ s12 + i22 * fd_next[..., jnp.newaxis, :]
+
+    return ScatteringMatrix(
+        s11=s11,
+        s12=s12,
+        s21=s21,
+        s22=s22,
+        start_layer_solve_result=layer_solve_result,
+        start_layer_thickness=layer_thickness,
+        end_layer_solve_result=next_layer_solve_result,
+        end_layer_thickness=next_layer_thickness,
+    )
 
 
 def set_end_layer_thickness(
